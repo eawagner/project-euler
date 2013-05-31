d7375995291125b30737d464c721cfeb906a1eec
eawagner <ceraw79@hotmail.com>
2013-05-31 16:01:55 -0400

    Fixed 24 to stop being so slow

--- src/main/java/practice/project/euler/problem/p20_29/Problem24.java
+++ src/main/java/practice/project/euler/problem/p20_29/Problem24.java
@@ -1,10 +1,7 @@
 package practice.project.euler.problem.p20_29;
 
 import practice.project.euler.Problem;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import practice.project.euler.util.GeneralUtil;
 
 /*
 A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
@@ -17,29 +14,31 @@ What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 
 public class Problem24 implements Problem{
     public String getAnswer() throws Exception {
 
-        boolean [] used = new boolean[10];
+        //First first digit has 9! permutations, second 8!, third 7! ...
+        //Since we are looking for the 1M th, simply find out which digit
+        //fits would exceed the threshold of what is left
+
+        long[] factorials = new long[10];
+        for (int i = 0;i< factorials.length;i++)
+            factorials[i] = GeneralUtil.getFactorial(i);
 
 
-        List<String> permutations = new ArrayList<String>(1000000);
-        setPermutations("",used,permutations,1000000);
+        long left = 1000000;
+        String answer = "";
 
-
-        return permutations.get(permutations.size()-1);
-    }
-
-    public void setPermutations(String currString, boolean[] available, Collection<String> permutations, int maxPermutations)
-    {
-        if (currString.length() >= available.length){
-            permutations.add(currString);
-            return;
+        while (answer.length() < 10) {
+            for (int i = 0; i< 10; i++) {
+                if (!answer.contains(Integer.toString(i))) {
+                    if (left - factorials[9 - answer.length()] > 0) {
+                        left = left - factorials[9 - answer.length()];
+                    } else {
+                        answer += Integer.toString(i);
+                        break;
+                    }
+                }
+            }
         }
 
-        for (int i = 0;i<10 && permutations.size()<maxPermutations;i++)
-            if (!available[i]) {
-
-                available[i] = true;
-                setPermutations(currString + Integer.toString(i),available,permutations,maxPermutations);
-                available[i] = false;
-            }
+        return answer;
     }
 }
